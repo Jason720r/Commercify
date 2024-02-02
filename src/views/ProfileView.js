3c1287dd66f30aa57192779cd55826ab8233c091
Jason Li <jasonli99193@gmail.com>
2024-02-02 14:29:12 -0600

    Profile component

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,2 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import React, { useEffect, useState } from 'react';
+import { userFetch } from "../managers/UserManager.js";
+import "./Profile.css"
+
+export const ProfileContainer = () => {
+    const [profile, setProfile] = useState([])
+}
