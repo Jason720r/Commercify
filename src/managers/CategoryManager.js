b90ea4fda57e809d870734ea2e199c970779f436
Jason Li <jasonli99193@gmail.com>
2024-02-06 07:38:36 -0600

    category fetch

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -8,5 +8,10 @@ export const categoryFetch = () => {
 }
 
 export const singleCategory = (categoryId) => {
-    return fetch(`http://localhost:8088/products/${categoryId}`,)
+    return fetch(`http://localhost:8088/products/${categoryId}`, {
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+    .then(response => response.json())
 }
