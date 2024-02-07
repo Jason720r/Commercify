5a5b2cba4adf5b093af138a36a7d18256f27b2ab
Jason Li <jasonli99193@gmail.com>
2024-02-07 12:35:14 -0600

    fixed Routing

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,13 +3,13 @@ import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
 import { ProductContainer } from "./ProductView.js"
+import { Navigate } from "react-router-dom"
 
 
 export const ApplicationViews = () => {
     return <>
         <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate to="/products" replace />} />
             <Route path="/products" element={<ProductContainer />} />
             <Route element={<Authorized />}>
                 {/* Add Routes here */}
