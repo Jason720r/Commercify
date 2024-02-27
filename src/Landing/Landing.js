ce432a6f77f17f80db9455aacae5e6da62d95616
Jason Li <jasonli99193@gmail.com>
2024-02-27 17:49:39 -0600

    styling applied

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LandingImage from './Shannon.JPG'
 import WebsiteTitle from './Title.png'
+import "./Landing.css";
 
 export const LandingPage = () => {
 
@@ -13,10 +14,10 @@ export const LandingPage = () => {
                 <a href='Contact'>Contact Me</a>
             </nav>
             <div className='client_container'>
+            <img src={LandingImage} alt="Shannon" className="landing_banner" />
             <div className='client_review'>"You bring a lot to the table, Shannon. Not only expertise, but a heart for healing and growth. You have a special gift to see women not just as they are, but who they can become. You gently and humbly reach inside them and pull that out."</div>
             </div>
             <div className='landing_description'>It's not just about your physical well-being, your mental health is just as important</div>
-            <img src={LandingImage} alt="Shannon" className="landing_logo" />
             <div className='service_container'>
                 <button className='service_shannon'>Diet and Coaching</button>
                 <button className='service_shannon'>Mental Health</button>
