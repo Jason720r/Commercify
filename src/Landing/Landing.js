61b6fb618dd3f921b33923dc6e34616a3fb996c0
Jason Li <jasonli99193@gmail.com>
2024-02-27 13:00:38 -0600

    client feedback

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import LandingImage from './Shannon.JPG'
+import WebsiteTitle from './Title.png'
 
 export const LandingPage = () => {
 
     return (
         <div className="landing_container">
-            <div className="landing_title"> Move, Heal, Love</div>
+
+            <img src={WebsiteTitle} alt="Web Logo" className='website_logo'/>
+            <nav>
+                <a href="About">About Me</a>
+                <a href='Contact'>Contact Me</a>
+            </nav>
+            <div className='client_container'>
+            <div className='client_review'>"You bring a lot to the table, Shannon. Not only expertise, but a heart for healing and growth. You have a special gift to see women not just as they are, but who they can become. You gently and humbly reach inside them and pull that out."</div>
+            </div>
             <div className='landing_description'>It's not just about your physical well-being, your mental health is just as important</div>
+            <img src={LandingImage} alt="Shannon" className="landing_logo" />
         </div>
     )
 }
