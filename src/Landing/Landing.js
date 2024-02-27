300513848c4a904ed2e8386d9f4f8daefce6056a
Jason Li <jasonli99193@gmail.com>
2024-02-27 11:37:55 -0600

    message created

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -4,6 +4,9 @@ import React from 'react';
 export const LandingPage = () => {
 
     return (
-        <div className="landing_container"></div>
+        <div className="landing_container">
+            <div className="landing_title"> Move, Heal, Love</div>
+            <div className='landing_description'>It's not just about your physical well-being, your mental health is just as important</div>
+        </div>
     )
 }
