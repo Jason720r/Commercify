d928f4d14c0485fdcfc9b76449f9efd2804c06d3
Jason Li <jasonli99193@gmail.com>
2024-02-29 11:31:04 -0600

    removed background

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -15,7 +15,7 @@ export const LandingPage = () => {
                 <a href='Contact'>Contact Me</a>
             </nav>
             <div className='client_container'>
-            {/* <img src={LandingImage} alt="Shannon" className="landing_banner" /> */}
+            <img src={LandingImage} alt="Shannon" className="landing_banner" />
             <div className='client_review'>"You bring a lot to the table, Shannon. Not only expertise, but a heart for healing and growth. You have a special gift to see women not just as they are, but who they can become. You gently and humbly reach inside them and pull that out."</div>
             </div>
             <div className='landing_description'>It's not just about your physical well-being, your mental health is just as important</div>
