b03694234f76ea8285d2244a5af88f131a4a2eac
Jason Li <jasonli99193@gmail.com>
2024-02-29 15:28:06 -0600

    description  added

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -27,7 +27,10 @@ export const LandingPage = () => {
             <img src={LandingImage} alt="Shannon" className="landing_banner" />
             <div className='client_review'>"You bring a lot to the table, Shannon. Not only expertise, but a heart for healing and growth. You have a special gift to see women not just as they are, but who they can become. You gently and humbly reach inside them and pull that out."</div>
             </div>
-            <div className='landing_description'>It's not just about your physical well-being, your mental health is just as important</div>
+            <div className='description_container'>
+                <div className='description_header'>It's not just about your physical well-being, your mental health is just as important</div>
+            <div className='landing_description'>Whether it be diet, coaching, or trauma, I'm here to support you through every step of your journey. Your body and mind are interconnected, and achieving balance between them is crucial for overall health. My approach is holistic, focusing on nutrition, exercise, and mental resilience to empower you to overcome challenges. Together, we'll develop a personalized plan that fits your lifestyle and goals, ensuring that you feel supported, motivated, and confident in your abilities. </div>
+            </div>
             <div className='service_container'>
                 <button className='service_shannon'>Diet and Coaching</button>
                 <button className='service_shannon'>Mental Health</button>
