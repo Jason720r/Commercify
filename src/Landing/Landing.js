0d9a1af86893074f9fbdf42cc373597cd7f2486d
Jason Li <jasonli99193@gmail.com>
2024-02-29 12:05:20 -0600

    logo and nav centered corrrectly

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import LandingImage from './Shannon.JPG'
-import McLogo from './McClure.png'
-import WebsiteTitle from './Title.png'
+import LandingImage from './Shannon.JPG';
+import McLogo from './McClure.png';
+import NoBackground from './Background.png';
+import WebsiteTitle from './Title.png';
 import "./Landing.css";
 
 export const LandingPage = () => {
 
     return (
         <div className="landing_container">
-
-            <img src={McLogo} alt="Web Logo" className='website_logo'/>
+            <div className='nav_container'>
             <nav>
                 <a href="About">About Me</a>
                 <a href='Contact'>Contact Me</a>
             </nav>
+            <img src={McLogo} alt="Web Logo" className='website_logo'/>
+            <nav>
+
+            </nav>
+            <nav>
+                
+            </nav>
+            </div>
             <div className='client_container'>
             <img src={LandingImage} alt="Shannon" className="landing_banner" />
             <div className='client_review'>"You bring a lot to the table, Shannon. Not only expertise, but a heart for healing and growth. You have a special gift to see women not just as they are, but who they can become. You gently and humbly reach inside them and pull that out."</div>
