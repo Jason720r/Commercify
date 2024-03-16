b6a57f330aeedf3b14d3b4372acca257df39dddb
Jason Li <jasonli99193@gmail.com>
2024-03-16 13:45:22 -0500

    nav links added

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -31,6 +31,8 @@ export const LandingPage = () => {
             <div className='description_container'>
                 <div className='description_header'>It's not just about your physical well-being, your mental health is just as important</div>
             <div className='landing_description'>Whether it be diet, coaching, or trauma, I'm here to support you through every step of your journey. Your body and mind are interconnected, and achieving balance between them is crucial for overall health. My approach is holistic, focusing on nutrition, exercise, and mental resilience to empower you to overcome challenges. Together, we'll develop a personalized plan that fits your lifestyle and goals, ensuring that you feel supported, motivated, and confident in your abilities. </div>
+            <div className='contact_link1'>Testimonials</div>
+            <div className='contact_link'>Get in Touch</div>
             </div>
             <div className="scroll_down_indicator"></div>
             <img src={Kettle} alt="Bell" className='kettle_image' />
