4d4cb3c48579663c3a932e352a00d6f04f33530f
Jason Li <jasonli99193@gmail.com>
2024-03-21 20:32:24 -0500

    new service

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -31,14 +31,17 @@ export const LandingPage = () => {
             <div className='description_container'>
                 <div className='description_header'>It's not just about your physical well-being, your mental health is just as important</div>
             <div className='landing_description'>Whether it be diet, coaching, or trauma, I'm here to support you through every step of your journey. Your body and mind are interconnected, and achieving balance between them is crucial for overall health. My approach is holistic, focusing on nutrition, exercise, and mental resilience to empower you to overcome challenges. Together, we'll develop a personalized plan that fits your lifestyle and goals, ensuring that you feel supported, motivated, and confident in your abilities. </div>
-            <div className='contact_link1'>Testimonials</div>
-            <div className='contact_link'>Get in Touch</div>
+            <div className='contact_container'>
+            {/* <button className='contact_link1'>Testimonials</button> */}
+            <button className='contact_link'>Get in Touch</button>
+            </div>
             </div>
             <div className="scroll_down_indicator"></div>
             <img src={Kettle} alt="Bell" className='kettle_image' />
             <div className='service_container'>
                 <button className='service_shannon'>Diet and Coaching</button>
                 <button className='service_shannon'>Mental Health</button>
+                <button className='service_shannon'>Motivation</button>
                 <button className='service_shannon'></button>
             </div>
         </div>
